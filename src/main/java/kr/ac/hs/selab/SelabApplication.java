30554da2884a38b90f2e65c45f9161cb8d791ddf
DongGeon0908 <50691225+DongGeon0908@users.noreply.github.com>
2022-01-04 01:55:38 +0900

    init

---
+++ src/main/java/kr/ac/hs/selab/SelabApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.hs.selab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/hs/selab/SelabApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.hs.selab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
