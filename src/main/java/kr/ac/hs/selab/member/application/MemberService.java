9d73dc1d91d3ceccecb740e0b045516905e9bbba
DongGeon0908 <50691225+DongGeon0908@users.noreply.github.com>
2022-01-04 22:24:21 +0900

    :sparkles: 중복된 값 검증

--- src/main/java/kr/ac/hs/selab/member/application/MemberService.java
+++ src/main/java/kr/ac/hs/selab/member/application/MemberService.java
@@ -1,12 +1,18 @@
 package kr.ac.hs.selab.member.application;
 
+import kr.ac.hs.selab.error.dto.ErrorMessage;
+import kr.ac.hs.selab.error.exception.common.DuplicationException;
 import kr.ac.hs.selab.member.converter.MemberConverter;
 import kr.ac.hs.selab.member.domain.Member;
+import kr.ac.hs.selab.member.domain.vo.Email;
+import kr.ac.hs.selab.member.domain.vo.Nickname;
+import kr.ac.hs.selab.member.domain.vo.StudentId;
 import kr.ac.hs.selab.member.dto.bundle.CreateMemberBundle;
 import kr.ac.hs.selab.member.dto.response.CreateMemberResponse;
 import kr.ac.hs.selab.member.infrastructure.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,9 +21,47 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final MemberConverter memberConverter;
 
+    @Transactional
     public CreateMemberResponse create(CreateMemberBundle bundle) {
+        isDuplication(bundle);
         Member instance = memberConverter.toMember(bundle);
         Member member = memberRepository.save(instance);
         return memberConverter.toCreateMemberResponse(member);
+    }
+
+    private void isDuplication(CreateMemberBundle bundle) {
+        isDuplicationEmail(bundle.getEmail());
+        isDuplicationStudentId(bundle.getStudentId());
+        isDuplicationNickname(bundle.getNickname());
+    }
+
+    private void isDuplicationEmail(Email email) {
+        if (existsByEmail(email)) {
+            throw new DuplicationException(ErrorMessage.MEMBER_EMAIL_DUPLICATION_ERROR);
+        }
+    }
+
+    private void isDuplicationStudentId(StudentId studentId) {
+        if (existsByStudentId(studentId)) {
+            throw new DuplicationException(ErrorMessage.MEMBER_STUDENT_ID_DUPLICATION_ERROR);
+        }
+    }
+
+    private void isDuplicationNickname(Nickname nickname) {
+        if (existsByNickname(nickname)) {
+            throw new DuplicationException(ErrorMessage.MEMBER_NICKNAME_DUPLICATION_ERROR);
+        }
+    }
+
+    private boolean existsByEmail(Email email) {
+        return memberRepository.existsByEmail(email);
+    }
+
+    private boolean existsByStudentId(StudentId studentId) {
+        return memberRepository.existsByStudentId(studentId);
+    }
+
+    private boolean existsByNickname(Nickname nickname) {
+        return memberRepository.existsByNickname(nickname);
     }
 }
--- src/main/java/kr/ac/hs/selab/member/infrastructure/MemberRepository.java
+++ src/main/java/kr/ac/hs/selab/member/infrastructure/MemberRepository.java
@@ -1,6 +1,9 @@
 package kr.ac.hs.selab.member.infrastructure;
 
 import kr.ac.hs.selab.member.domain.Member;
+import kr.ac.hs.selab.member.domain.vo.Email;
+import kr.ac.hs.selab.member.domain.vo.Nickname;
+import kr.ac.hs.selab.member.domain.vo.StudentId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +12,10 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Override
     <S extends Member> S save(S entity);
+
+    boolean existsByEmail(Email email);
+
+    boolean existsByStudentId(StudentId studentId);
+
+    boolean existsByNickname(Nickname nickname);
 }
