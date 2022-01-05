ca6bea203c5efb2fedbe633ef2d677a7ca8a5cd3
DongGeon0908 <50691225+DongGeon0908@users.noreply.github.com>
2022-01-05 16:30:35 +0900

    :sparkles: 기본적인 Security Setting 작업 진행

--- src/main/java/kr/ac/hs/selab/common/config/PropertiesConfig.java
+++ src/main/java/kr/ac/hs/selab/common/config/PropertiesConfig.java
@@ -1,6 +1,5 @@
 package kr.ac.hs.selab.common.config;
 
-import kr.ac.hs.selab.common.properties.CorsProperties;
 import kr.ac.hs.selab.common.properties.JwtProperties;
 import kr.ac.hs.selab.common.properties.SwaggerProperties;
 import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
--- src/main/java/kr/ac/hs/selab/common/config/SecurityConfig.java
+++ src/main/java/kr/ac/hs/selab/common/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -33,6 +34,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         httpSecurity
             .httpBasic().disable();
+
+        httpSecurity
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Bean
