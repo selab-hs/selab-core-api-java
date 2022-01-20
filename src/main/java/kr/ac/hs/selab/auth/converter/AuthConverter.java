d069f202ea405946575ca4186f61b026220a5f63
DongGeon0908 <50691225+DongGeon0908@users.noreply.github.com>
2022-01-21 02:51:55 +0900

    :recycle: 컨버터 수정 진행

--- src/main/java/kr/ac/hs/selab/auth/converter/AuthConverter.java
+++ src/main/java/kr/ac/hs/selab/auth/converter/AuthConverter.java
@@ -1,22 +1,20 @@
 package kr.ac.hs.selab.auth.converter;
 
-import kr.ac.hs.selab.auth.dto.response.LoginResponse;
+import kr.ac.hs.selab.auth.dto.response.AuthLoginResponse;
 import kr.ac.hs.selab.member.domain.Member;
-import kr.ac.hs.selab.member.domain.vo.Role;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.stereotype.Component;
 
-@Component
 public class AuthConverter {
 
-    public LoginResponse toLoginResponse(Authentication authentication, String token) {
-        return new LoginResponse(authentication.getName(), token);
+    public static AuthLoginResponse toAuthLoginResponse(Authentication authentication,
+        String token) {
+        return new AuthLoginResponse(authentication.getName(), token);
     }
 
-    public User toUser(Member member) {
+    public static User toUser(Member member) {
         return new User(
-            member.getEmailValue(),
+            member.getEmail(),
             member.getPasswordValue(),
             member.getAuthority()
         );
