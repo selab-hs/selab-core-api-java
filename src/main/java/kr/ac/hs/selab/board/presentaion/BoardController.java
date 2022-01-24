1819a5baa2129ecdf6013cbcafeddc0502a62d95
DongGeon0908 <50691225+DongGeon0908@users.noreply.github.com>
2022-01-24 22:39:07 +0900

    :bug: 반환타입 수정

--- src/main/java/kr/ac/hs/selab/board/presentaion/BoardController.java
+++ src/main/java/kr/ac/hs/selab/board/presentaion/BoardController.java
@@ -10,7 +10,6 @@ import kr.ac.hs.selab.board.dto.response.BoardsResponse;
 import kr.ac.hs.selab.common.template.ResponseMessage;
 import kr.ac.hs.selab.common.template.ResponseTemplate;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -19,43 +18,44 @@ import javax.validation.Valid;
 @RequestMapping(value = "api/v1/boards")
 @RestController
 public class BoardController implements BoardSdk {
+
     private final BoardService boardService;
 
     @Override
     @PostMapping
-    public ResponseEntity<ResponseTemplate<BoardResponse>> create(@Valid @RequestBody BoardRequest request) {
+    public ResponseTemplate<BoardResponse> create(@Valid @RequestBody BoardRequest request) {
         BoardCreateDto dto = BoardCreateDto.of(request.getTitle(), request.getDescription());
         BoardResponse response = boardService.create(dto);
-        return ResponseTemplate.of(ResponseMessage.BOARD_CREATE_SUCCESS, response);
+        return ResponseTemplate.created(ResponseMessage.BOARD_CREATE_SUCCESS, response);
     }
 
     @Override
     @GetMapping("/{id}")
-    public ResponseEntity<ResponseTemplate<BoardResponse>> find(@PathVariable Long id) {
+    public ResponseTemplate<BoardResponse> find(@PathVariable Long id) {
         BoardResponse response = boardService.find(id);
-        return ResponseTemplate.of(ResponseMessage.BOARD_FIND_SUCCESS, response);
+        return ResponseTemplate.ok(ResponseMessage.BOARD_FIND_SUCCESS, response);
     }
 
     @Override
     @GetMapping
-    public ResponseEntity<ResponseTemplate<BoardsResponse>> findAll() {
+    public ResponseTemplate<BoardsResponse> findAll() {
         BoardsResponse response = boardService.findAll();
-        return ResponseTemplate.of(ResponseMessage.BOARD_FIND_SUCCESS, response);
+        return ResponseTemplate.ok(ResponseMessage.BOARD_FIND_SUCCESS, response);
     }
 
     @Override
     @PutMapping("/{id}")
-    public ResponseEntity<ResponseTemplate<BoardResponse>> update(@PathVariable Long id,
-                                                                  @Valid @RequestBody BoardRequest request) {
+    public ResponseTemplate<BoardResponse> update(@PathVariable Long id,
+        @Valid @RequestBody BoardRequest request) {
         BoardUpdateDto dto = BoardConverter.toBoardUpdateDto(id, request);
         BoardResponse response = boardService.update(dto);
-        return ResponseTemplate.of(ResponseMessage.BOARD_FIND_SUCCESS, response);
+        return ResponseTemplate.ok(ResponseMessage.BOARD_FIND_SUCCESS, response);
     }
 
     @Override
     @PatchMapping("/{id}")
-    public ResponseEntity<ResponseTemplate<BoardResponse>> delete(@PathVariable Long id) {
+    public ResponseTemplate<BoardResponse> delete(@PathVariable Long id) {
         BoardResponse response = boardService.delete(id);
-        return ResponseTemplate.of(ResponseMessage.BOARD_DELETE_SUCCESS, response);
+        return ResponseTemplate.ok(ResponseMessage.BOARD_DELETE_SUCCESS, response);
     }
 }
