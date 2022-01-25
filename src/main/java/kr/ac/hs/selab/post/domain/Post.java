7538c5b0abc7a14c527b99ea501b29387916c590
leeheechan <leeheefull@gmail.com>
2022-01-25 17:30:14 +0900

    feat(:sparkles:) - implement post entity

---
+++ src/main/java/kr/ac/hs/selab/post/domain/Post.java
@@ -0,0 +1,61 @@
+package kr.ac.hs.selab.post.domain;
+
+import kr.ac.hs.selab.board.domain.Board;
+import kr.ac.hs.selab.common.domain.BaseEntity;
+import kr.ac.hs.selab.common.utils.Constants;
+import kr.ac.hs.selab.member.domain.Member;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Post extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @Column(name = "post_title")
+    private String title;
+
+    @Column(name = "post_content")
+    private String content;
+
+    @Column(name = "post_delete_flag")
+    private boolean deleteFlag;
+
+    @Builder
+    private Post(Member member, Board board, String title, String content) {
+        this.member = member;
+        this.board = board;
+        this.title = title;
+        this.content = content;
+        this.deleteFlag = false;
+    }
+
+    public Post update(String title, String content) {
+        this.title = title;
+        this.content = content;
+        return this;
+    }
+
+    public Post delete() {
+        this.title = this.title + Constants.TITLE_SEPARATOR + UUID.randomUUID();
+        this.deleteFlag = true;
+        return this;
+    }
+}
