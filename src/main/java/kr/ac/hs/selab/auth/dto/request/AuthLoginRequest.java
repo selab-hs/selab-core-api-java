20f21875ddae8a911d69463d3eda6d04c3f29a0c
leeheechan <leeheefull@gmail.com>
2022-01-29 20:07:27 +0900

    fix(:bug:) - request final 변수 제거

--- src/main/java/kr/ac/hs/selab/auth/dto/request/AuthLoginRequest.java
+++ src/main/java/kr/ac/hs/selab/auth/dto/request/AuthLoginRequest.java
@@ -1,18 +1,17 @@
 package kr.ac.hs.selab.auth.dto.request;
 
+import lombok.Getter;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class AuthLoginRequest {
-
     @NotNull
     @Email
-    private final String email;
+    private String email;
+
     @NotBlank
-    private final String password;
+    private String password;
 }
