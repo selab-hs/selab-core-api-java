c3f60cbb7a8919236b4ac3cb0fae2ca228f9150e
leeheechan <leeheefull@gmail.com>
2022-01-30 14:23:16 +0900

    🐛 [fix/member-52] Request object error (#54)
    fix(:bug:) - request final 변수 제거

--- src/main/java/kr/ac/hs/selab/member/dto/request/MemberCreateRequest.java
+++ src/main/java/kr/ac/hs/selab/member/dto/request/MemberCreateRequest.java
@@ -1,28 +1,27 @@
 package kr.ac.hs.selab.member.dto.request;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+
+import javax.validation.constraints.*;
 
 @Getter
-@RequiredArgsConstructor
 public class MemberCreateRequest {
-
     @NotNull
     @Email
-    private final String email;
+    private String email;
+
     @NotBlank
-    private final String password;
+    private String password;
+
     @NotBlank
-    private final String studentId;
+    private String studentId;
+
     @Pattern(regexp = "^[가-힣]{2,10}$")
-    private final String name;
+    private String name;
+
     @NotBlank
     @Size(max = 15)
-    private final String nickname;
-    private final String avatar;
+    private String nickname;
+
+    private String avatar;
 }
