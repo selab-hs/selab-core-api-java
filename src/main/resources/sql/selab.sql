233eb8e4ce74be2ae3635bbf79353185636fcc6f
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2022-02-16 15:04:02 +0900

    :bug: 공통 property 수정 작업 진행

--- src/main/resources/sql/selab.sql
+++ src/main/resources/sql/selab.sql
@@ -1,2 +1,15 @@
 # CREATE DATABASE
-create database wordpress character set utf8mb4 collate utf8mb4_general_ci;
+create database selab character set utf8mb4 collate utf8mb4_general_ci;
+
+#데이터베이스 선택하기
+use selab;
+
+#테이블 구조보기
+desc member;
+desc board;
+desc comment;
+desc likes;
+desc post;
+desc terms;
+
+show tables;
