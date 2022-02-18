147ea60a257a12ab0ff28bca5c38598d60ace3e6
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2022-02-19 01:26:22 +0900

    build error 수정

--- src/test/java/kr/ac/hs/selab/SelabApplicationTests.java
+++ src/test/java/kr/ac/hs/selab/SelabApplicationTests.java
@@ -1,13 +1,7 @@
 package kr.ac.hs.selab;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SelabApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
