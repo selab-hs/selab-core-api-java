28df642e7339ef3be875adceb9c362bd547061ad
leeheechan <leeheefull@gmail.com>
2022-02-20 17:36:07 +0900

    refactor(:recycle:) - 게시판 삭제 시 게시판과 관련된 게시글, 게시글 좋아요, 댓글, 댓글 좋아요 모두 삭제

--- src/main/java/kr/ac/hs/selab/board/eventListener/BoardEventListener.java
+++ src/main/java/kr/ac/hs/selab/board/eventListener/BoardEventListener.java
@@ -2,28 +2,41 @@ package kr.ac.hs.selab.board.eventListener;
 
 import kr.ac.hs.selab.board.domain.event.BoardEvent;
 import kr.ac.hs.selab.comment.application.CommentService;
+import kr.ac.hs.selab.comment.domain.Comment;
+import kr.ac.hs.selab.commentLike.application.CommentLikeService;
 import kr.ac.hs.selab.post.application.PostService;
+import kr.ac.hs.selab.post.domain.Post;
+import kr.ac.hs.selab.postLike.application.PostLikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Component
 public class BoardEventListener {
     private final PostService postService;
     private final CommentService commentService;
+    private final PostLikeService postLikeService;
+    private final CommentLikeService commentLikeService;
 
     @Async
     @Transactional(value = Transactional.TxType.REQUIRES_NEW)
     @TransactionalEventListener
     public void deleteByBoard(BoardEvent boardEvent) {
         var board = boardEvent.getBoard();
-        commentService.deleteByPosts(
-                postService.findPostsByBoard(board)
-        );
+        List<Post> posts = postService.findPostsByBoard(board);
+
+        posts.forEach(post -> {
+            List<Comment> comments = commentService.findCommentsByPost(post);
+            commentLikeService.deleteByComments(comments);
+        });
+        commentService.deleteByPosts(posts);
+
+        posts.forEach(postLikeService::deleteByPost);
         postService.deleteByBoard(board);
     }
 }
