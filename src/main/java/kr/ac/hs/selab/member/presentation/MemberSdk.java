9bc5b1663bd093c298d4e3c9a09b033d67bdfbc2
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2022-02-26 02:01:34 +0900

    :recycle: swagger notes 추가

--- src/main/java/kr/ac/hs/selab/member/presentation/MemberSdk.java
+++ src/main/java/kr/ac/hs/selab/member/presentation/MemberSdk.java
@@ -4,17 +4,16 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import kr.ac.hs.selab.common.template.ResponseTemplate;
+import kr.ac.hs.selab.common.template.SwaggerNoteKt;
 import kr.ac.hs.selab.member.dto.request.MemberCreateRequest;
 import kr.ac.hs.selab.member.dto.response.MemberCreateResponse;
-
 
 @Api(tags = "Member REST API")
 public interface MemberSdk {
 
-    @ApiOperation(value = "회원가입", notes = "todo") // TODO notes에는 request header와 request body, response body를 추가
+    @ApiOperation(value = "회원가입", notes = SwaggerNoteKt.MEMBER_INSERT)
     @ApiResponses(
             @ApiResponse(code = 201, message = "회원가입 성공")
     )
