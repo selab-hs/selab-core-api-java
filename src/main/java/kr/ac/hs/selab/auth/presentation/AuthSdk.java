972c887f5511bc2ad471e2619b95671622ece5e9
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2022-03-05 01:57:08 +0900

    :zap: Swagger 수정

--- src/main/java/kr/ac/hs/selab/auth/presentation/AuthSdk.java
+++ src/main/java/kr/ac/hs/selab/auth/presentation/AuthSdk.java
@@ -1,23 +1,20 @@
 package kr.ac.hs.selab.auth.presentation;
 
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.annotations.*;
 import kr.ac.hs.selab.auth.dto.request.AuthLoginRequest;
 import kr.ac.hs.selab.auth.dto.response.AuthLoginResponse;
 import kr.ac.hs.selab.common.template.ResponseTemplate;
+import kr.ac.hs.selab.common.template.SwaggerNote;
 
 @Api(tags = "Auth REST API", description = "인증 및 인가 api")
 public interface AuthSdk {
 
-    @Operation(summary = "로그인", description = "아이디와 비밀번호를 통해 로그인을 진행합니다.")
+    @ApiOperation(value = "로그인", notes = SwaggerNote.AUTH_LOGIN)
     @ApiResponses({
-        @ApiResponse(code = 200, message = "로그인 성공")
+            @ApiResponse(code = 200, message = "로그인 성공")
     })
     ResponseTemplate<AuthLoginResponse> login(
-        @ApiParam(value = "이메일과 비밀번호")
-            AuthLoginRequest request);
+            @ApiParam(value = "이메일과 비밀번호")
+                    AuthLoginRequest request);
 }
--- src/main/java/kr/ac/hs/selab/common/template/SwaggerNote.java
+++ src/main/java/kr/ac/hs/selab/common/template/SwaggerNote.java
@@ -8,20 +8,41 @@ public class SwaggerNote {
             "\n" +
             "Request Body\n" +
             "{\n" +
-            "   \"email\" : \"wrjs@naver.com\", // 이메일\n" +
-            "   \"password\" : \"sdputd2423\", // 비밀번호 \n" +
-            "   \"studentId\" : \"201658109\", // 학번\n" +
-            "   \"name\" : \"김동건\", // 김동건\n" +
-            "   \"nickname\" : \"야생의 구피\" // 닉네임\n" +
+            "\"email\" : \"wrjs@naver.com\", // 이메일\n" +
+            "\"password\" : \"sdputd2423\", // 비밀번호 \n" +
+            "\"studentId\" : \"201658109\", // 학번\n" +
+            "\"name\" : \"김동건\", // 김동건\n" +
+            "\"nickname\" : \"야생의 구피\" // 닉네임\n" +
             "}\n" +
             "\n" +
             "Response Body\n" +
             "{\n" +
-            "   \"message\": \"MEMBER_INSERT_SUCCESS\", // api name\n" +
-            "   \"code\": \"R-M-0001\", // api code\n" +
-            "   \"serverDateTime\": \"2022-02-18T22:53:46.270418\", // server date time\n" +
-            "   \"data\": {\n" +
-            "       \"email\": \"wrjs@naver.com\" // 이메일\n" +
-            "     }\n" +
+            "\"message\": \"MEMBER_INSERT_SUCCESS\", // api name\n" +
+            "\"code\": \"R-M-0001\", // api code\n" +
+            "\"serverDateTime\": \"2022-02-18T22:53:46.270418\", // server date time\n" +
+            "\"data\": {\n" +
+            "\"email\": \"wrjs@naver.com\" // 이메일\n" +
+            "}\n" +
+            "}";
+
+    public static final String AUTH_LOGIN = "" +
+            "로그인\n" +
+            "\n" +
+            "POST /api/v1/auth/login\n" +
+            "\n" +
+            "Request Body\n" +
+            "{\n" +
+            "\"email\" : \"wrjs@naver.com\", // 이메일\n" +
+            "\"password\" : \"dsk3sa4356klsadk\" // 비밀번호\n" +
+            "}\n" +
+            "\n" +
+            "Response Body\n" +
+            "{\n" +
+            "\"code\" : \";asld;lksakd;laskd;lak;dlklas;dkas;dkas\"" +
+            "\"data\": {" +
+            "\"email\": \"wrjs@naver.com\"" +
+            "}," +
+            "\"message\": \"string\",\n" +
+            "\"serverDateTime\": \"2022-03-04T15:21:28.416Z\"" +
             "}";
 }
