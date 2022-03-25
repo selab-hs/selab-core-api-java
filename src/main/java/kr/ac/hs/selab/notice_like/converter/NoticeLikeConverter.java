bb1e4999cdc99a4e526082e8fa680b0263084d2b
leeheechan <leeheefull@gmail.com>
2022-03-25 21:14:36 +0900

    :sparkles: 공지사항 좋아요 facade 구현

---
+++ src/main/java/kr/ac/hs/selab/notice_like/converter/NoticeLikeConverter.java
@@ -0,0 +1,27 @@
+package kr.ac.hs.selab.notice_like.converter;
+
+import kr.ac.hs.selab.notice_like.domain.NoticeLike;
+import kr.ac.hs.selab.notice_like.dto.response.NoticeLikeFindResponse;
+import lombok.experimental.UtilityClass;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@UtilityClass
+public class NoticeLikeConverter {
+    public NoticeLikeFindResponse toNoticeLikeFindResponse(Long noticeId, List<NoticeLike> likes) {
+        return NoticeLikeFindResponse.builder()
+                .noticeId(noticeId)
+                .totalCount((long) likes.size())
+                .noticeLikes(
+                        likes.stream()
+                                .map(NoticeLikeConverter::toNoticeLikeInnerResponse)
+                                .collect(Collectors.toList())
+                )
+                .build();
+    }
+
+    private NoticeLikeFindResponse.NoticeLikeInnerResponse toNoticeLikeInnerResponse(NoticeLike like) {
+        return new NoticeLikeFindResponse.NoticeLikeInnerResponse(like.getId(), like.getMemberId());
+    }
+}
---
+++ src/main/java/kr/ac/hs/selab/notice_like/facade/NoticeLikeFacade.java
@@ -0,0 +1,39 @@
+package kr.ac.hs.selab.notice_like.facade;
+
+import kr.ac.hs.selab.member.application.MemberService;
+import kr.ac.hs.selab.notice_like.application.NoticeLikeService;
+import kr.ac.hs.selab.notice_like.converter.NoticeLikeConverter;
+import kr.ac.hs.selab.notice_like.dto.NoticeLikeDto;
+import kr.ac.hs.selab.notice_like.dto.response.NoticeLikeFindResponse;
+import kr.ac.hs.selab.notice_like.dto.response.NoticeLikeResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Component
+public class NoticeLikeFacade {
+    private final MemberService memberService;
+    private final NoticeLikeService noticeLikeService;
+
+    @Transactional
+    public NoticeLikeResponse create(String memberEmail, Long noticeId) {
+        var memberId = memberService.findByEmail(memberEmail).getId();
+        var dto = new NoticeLikeDto(memberId, noticeId);
+
+        var like = noticeLikeService.create(dto);
+        return new NoticeLikeResponse(like.getId());
+    }
+
+    public NoticeLikeFindResponse find(Long noticeId) {
+        var likes = noticeLikeService.find(noticeId);
+        return NoticeLikeConverter.toNoticeLikeFindResponse(noticeId, likes);
+    }
+
+    @Transactional
+    public NoticeLikeResponse delete(Long noticeId) {
+        noticeLikeService.deleteByNoticeLikeId(noticeId);
+        return new NoticeLikeResponse(noticeId);
+    }
+}
