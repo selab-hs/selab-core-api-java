243a8743e17bef7d2527e45c689057692c4381e1
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2022-03-28 22:45:23 +0900

    :recycle: record 변환

--- src/main/java/kr/ac/hs/selab/coreQa/dto/bundle/CoreQaCreateBundle.java
+++ src/main/java/kr/ac/hs/selab/coreQa/dto/bundle/CoreQaCreateBundle.java
@@ -1,12 +1,4 @@
 package kr.ac.hs.selab.coreQa.dto.bundle;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
-public class CoreQaCreateBundle {
-    private final String email;
-    private final String title;
-    private final String content;
+public record CoreQaCreateBundle(String email, String title, String content) {
 }
